ff678a8feaf295283f939cba134b8b398a9a1580
YasserYka <yasseryka@gmail.com>
2021-01-24 19:00:16 +0300

    setup

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -2,13 +2,145 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import Car, Person, setup_db
+from auth.auth import AuthError, requires_auth
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+    # create and configure the app
+    app = Flask(__name__)
+    CORS(app)
+    setup_db(app)
 
-  return app
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type, Authorization"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET, PATCH, POST, DELETE, OPTIONS"
+        )
+        return response
+
+    @app.route('/persons', methods=['GET'])
+    @requires_auth('get:persons')
+    def get_persons(token):
+
+        persons = Person.query.all()
+
+        if len(persons) == 0:
+            abort(404)
+
+        return {'persons': [person.json() for person in persons], 'success': True}
+
+    @app.route('/cars', methods=['GET'])
+    @requires_auth('get:cars')
+    def get_cars(token):
+
+        cars = Car.query.all()
+
+        if len(cars) == 0:
+            abort(404)
+
+        return {'cars': [car.json() for car in cars],
+                'success': True}
+
+    @app.route('/cars', methods=['POST'])
+    @requires_auth('post:cars')
+    def create_car(token):
+
+        body = request.get_json()
+
+        (color, release, person_name) = (body.get('color'), body.get('release'), body.get('person_name'))
+
+        person = Person.query.filter(Person.name == person_name).one_or_none()
+
+        if color is None or release is None or person is None:
+            abort(422)
+
+        car = Car(release=release, color=color)
+        
+        person.cars.append(car)
+
+        person.update()
+
+        return {'cars': [car.json()], 'success': True}
+
+
+    @app.route('/persons', methods=['POST'])
+    @requires_auth('post:persons')
+    def create_person(token):
+
+        body = request.get_json()
+
+        (name, age) = (body.get('name'), body.get('age'))
+
+        if name is None or age is None:
+            abort(422)
+
+        person = Person(name=name, age=age)
+        person.insert()
+
+        return {'persons': [person.json()], 'success': True}
+
+    @app.route('/persons/<int:id>', methods=['DELETE'])
+    @requires_auth('delete:persons')
+    def delete_person(token, id):
+
+        person = Person.query.filter(Person.id == id).one_or_none()
+
+        if person is None:
+            abort(404)
+
+        person.delete()
+
+        return jsonify({'success': True, 'deleted': id})
+
+    @app.route('/persons/<int:id>', methods=['PATCH'])
+    @requires_auth('patch:persons')
+    def update_person(token, id):
+
+        person = Person.query.filter(Person.id == id).one_or_none()
+
+        if person is None:
+            abort(404)
+
+        body = request.get_json()
+
+        (name, age) = (body.get('name'), body.get('age'))
+
+        if name is not None:
+            person.name = name
+
+        if age is not None:
+            person.age = age
+
+        person.update()
+
+        return {'persons': [person.json()], 'success': True}
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return (jsonify({'success': False, 'error': 422,
+                'message': 'unprocessable'}), 422)
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return (jsonify({'success': False, 'error': 404,
+                'message': 'Resource not found'}), 404)
+
+    @app.errorhandler(401)
+    def unauthorized(error):
+        return (jsonify({'success': False, 'error': 401,
+                'message': 'Not Authorized'}), 401)
+
+    @app.errorhandler(403)
+    def unauthorized(error):
+        return (jsonify({'success': False, 'error': 403,
+                'message': 'Forbidden'}), 403)
+    
+    return app
 
 APP = create_app()
 
--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -1,1 +1,77 @@
 
+import os
+from sqlalchemy import Column, String, Integer
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+database_filename = "database.db"
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+
+db = SQLAlchemy()
+
+def setup_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+
+class Car(db.Model):
+    id = Column(Integer().with_variant(Integer, "sqlite"), primary_key=True)
+
+    color = Column(String(80))
+
+    person_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+
+    release =  Column(String(4), nullable=False)
+
+    def json(self):
+
+        return {
+            'id': self.id,
+            'color': self.color,
+            'release': self.release
+        }
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+class Person(db.Model):
+    id = Column(Integer().with_variant(Integer, "sqlite"), primary_key=True)
+
+    name = Column(String(80), unique=True)
+
+    cars = db.relationship("Car", backref=db.backref('person', lazy=True))
+
+    age =  Column(Integer(), nullable=False)
+
+    def json(self):
+
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+        }
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
